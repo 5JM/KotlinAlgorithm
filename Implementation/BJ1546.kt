e18ac4dbffd3fb5f3f252de863ac8ac67f64e5fe
5JM <toddlf0614@naver.com>
2022-05-16 00:55:57 +0900

    update:백준1546 - 평균

--- Implementation/BJ1546.kt
+++ Implementation/BJ1546.kt
@@ -1,2 +1,14 @@
 package Implementation
 
+import java.util.Scanner
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    val n = readLine()!!.toInt()
+    val t = Array(n){sc.nextInt()}
+    val arr = ArrayList<Double>()
+    for(i in 0 until n) arr.add(t[i].toDouble())
+    val max =arr.maxOrNull()!!
+    for(i in arr.indices) arr[i] = arr[i] / max *100
+    print(arr.sum()/arr.size)
+}
