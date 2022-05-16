22d6dfc89fa22ac6ee5e21f28b7adb78f0f4c23d
5JM <toddlf0614@naver.com>
2022-05-16 17:16:17 +0900

    add:백준4344 - 평군은 넘겠지

---
+++ Implementation/BJ4344.kt
@@ -0,0 +1,31 @@
+package Implementation
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+// 평균은 넘겠지
+fun main(){
+	val br = BufferedReader(InputStreamReader(System.`in`))
+	val c = br.readLine().toInt() // 케이스 개수
+
+	for (i in 0 until c) {
+		val str = StringTokenizer(br.readLine())
+		val n = str.nextToken().toInt() // 처음 나온 숫자가 학생 수
+		val arr = IntArray(n) // 학생 수 만큼 배열 생성
+		var sum = 0
+		var cnt = 0
+
+		for (j in 1..n) {
+			val token = str.nextToken().toInt()
+			sum += token
+			arr[j-1] = token
+		}
+
+		for (k in arr)
+			if (k > sum/n) cnt++ // 평균 넘는 학생 수 카운팅
+
+		println(String.format("%.3f%%",100f*cnt/n))
+	}
+	br.close()
+}
