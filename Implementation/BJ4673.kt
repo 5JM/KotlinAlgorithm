5e9b4f33e36f08ed88e8f712cc25d16dea4ac0aa
5JM <toddlf0614@naver.com>
2022-05-17 23:10:08 +0900

    add:백준4673 - 셀프넘버

---
+++ Implementation/BJ4673.kt
@@ -0,0 +1,12 @@
+package Implementation
+
+fun d(n:Int):Int{
+    var sum = n
+    for(i in n.toString()) sum+=i.digitToInt()
+    return sum
+}
+fun main(){
+    val arr = ArrayList<Int>()
+    for(i in 1..10000) arr.add(d(i))
+    for(i in 1..10000) if(!arr.contains(i)) println(i)
+}
