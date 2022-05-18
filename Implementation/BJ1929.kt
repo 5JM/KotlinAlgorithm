bcd8d1052c4023463e80bf064276e363af9fc86a
5JM <toddlf0614@naver.com>
2022-05-18 15:54:40 +0900

    add:백준1929 - 에라토스테네스 소수찾기

---
+++ Implementation/BJ1929.kt
@@ -0,0 +1,33 @@
+package Implementation
+
+import java.util.*
+
+private fun fastSosu(a : Int, b : Int){
+    // 소수인지 저장하는 array
+    val primeArr = BooleanArray(b + 1){true}
+    val sqrt = kotlin.math.sqrt(b.toDouble()).toInt()
+    // sqrt의 배수를 채로 거름
+    for (i in 2..sqrt){
+        // 이미 지워진 경우(4, 6 의 배수 등등)
+        if (!primeArr[i]) continue
+        // 소수의 배수 제외
+        else {
+            var j = 2
+            while (i*j <= b) {
+                if (primeArr[i * j]) primeArr[i * j] = false
+                j++
+            }
+        }
+    }
+
+    for (i in a..b){
+        if (i == 0 || i == 1) continue
+        if (primeArr[i]) println(i)
+    }
+}
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    val arr = Array(2){sc.nextInt()}
+    fastSosu(arr[0], arr[1])
+}
